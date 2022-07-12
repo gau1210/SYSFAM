b84763a45e48d8a40b8758a07505626f6da0365c
glaub <gau1210@gmail.com>
2022-07-11 23:55:46 -0300

    Não aguento mais, quero me formar.

--- app/src/main/java/com/example/sysfam/activity/MainActivity.java
+++ app/src/main/java/com/example/sysfam/activity/MainActivity.java
@@ -32,7 +32,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.ktx.Firebase;
+
 
 public class MainActivity extends AppCompatActivity {
 
